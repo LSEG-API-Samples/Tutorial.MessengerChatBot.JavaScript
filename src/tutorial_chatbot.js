d3f0c6665240dd74772d08a3fca2756f2ad28396
Wasin Waeosri <wasin.waeosri@refinitiv.com>
2019-11-29 16:01:50 +0700

    Change logs: 1. Update README.md 2. Update license 3. Update readme.txt 4. Update code license

--- src/tutorial_chatbot.js
+++ src/tutorial_chatbot.js
@@ -1,6 +1,15 @@
+//|-----------------------------------------------------------------------------
+//|            This source code is provided under the Apache 2.0 license      --
+//|  and is provided AS IS with no warranty or guarantee of fit for purpose.  --
+//|                See the project's LICENSE.md for details.                  --
+//|              Copyright Refinitiv 2019. All rights reserved.               --
+//|-----------------------------------------------------------------------------
+
 const request = require("request-promise");
 var WebSocketClient = require("websocket").client;
 const Timeout = ms => new Promise(res => setTimeout(res, ms));
+
+// Global Default Variables
 var text = "";
 
 //Assign variables for pre-production & production.
